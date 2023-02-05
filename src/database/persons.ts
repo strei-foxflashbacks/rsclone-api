0a6b0442f035c21917d6366e736507a616d2341e
ssekridov@gmail.com <ssekridov@gmail.com>
2023-02-05 14:43:01 +0100

    feat: person database

---
+++ src/database/persons.ts
@@ -0,0 +1,15 @@
+import { Person } from '../types/Person';
+
+const persons: Person[] = [
+  {
+    id: 1,
+    img: 'https://pic.showjet.ru/pics/932b74ff-6252-43f7-85e4-b85aea3e7102/10x5/1250.jpg',
+    imgMin: 'https://pic.showjet.ru/pics/932b74ff-6252-43f7-85e4-b85aea3e7102/10x10/100.jpg',
+    nameRu: 'Стив Пембертон',
+    nameEn: 'Steve Pemberton',
+    profession: 'актёр, режиссёр, сценарист, продюсер',
+    birth: 'Дата рождения 01 сентября 1967 г., Блэкберн, Ланкашир, Англия, Великобритания',
+    filmIds: [2],
+  },
+];
+export default persons;
---
+++ src/types/Person.ts
@@ -0,0 +1,10 @@
+export type Person = {
+  id: number,
+  img: string,
+  imgMin: string,
+  nameRu: string,
+  nameEn: string,
+  profession: string,
+  birth: string,
+  filmIds: number[],
+};
