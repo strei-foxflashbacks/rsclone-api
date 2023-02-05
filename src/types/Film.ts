907b6051753544b83a5082f051d2258d5fa66750
ssekridov@gmail.com <ssekridov@gmail.com>
2023-02-05 14:03:39 +0100

    feat: complete database items

--- src/types/Film.ts
+++ src/types/Film.ts
@@ -1,8 +1,10 @@
+import { Role } from './Role';
 import { Serial } from './Serial';
 
 export type Film = {
   id: number,
   name: string,
+  originalName: string,
   poster: string,
   trailer: string,
   thumbnails: string[],
@@ -10,7 +12,16 @@ export type Film = {
   rating: number,
   reviews: string[],
   genre: string,
+  release: string[],
+  audio: string,
+  studio: string,
+  countries: string,
+  actors: Role[],
+  director: Role[],
+  producers: Role[],
+  facts: string[],
   type: 'film' | 'serial',
+  wins?: string,
   serial?: Serial
   filmSrc?: string,
 };
---
+++ src/types/Role.ts
@@ -0,0 +1,5 @@
+export type Role = {
+  img: string,
+  name: string,
+  role: string,
+};
